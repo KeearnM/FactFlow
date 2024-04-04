7b4be0979324c176417df703ecd7d28a271bb52a
samanthatan01 <samanthatan6496@gmail.com>
2024-04-04 08:57:58 +0800

    Create SmartCollections Backend
    Create controllers, models and routers

--- backend/src/controllers/smartCollections.js
+++ backend/src/controllers/smartCollections.js
@@ -1,3 +1,71 @@
 const SmartCollectionsModel = require("../models/SmartCollections");
+// import users model
 
-module.exports = {};
+// --------------------------------------------------------------- //
+
+// seed collection
+const seedSmartCollection = async (req, res) => {
+  try {
+    await SmartCollectionsModel.deleteMany({});
+
+    await SmartCollectionsModel.create([
+      {
+        topic: "Market",
+      },
+      {
+        topic: "Tech Startups",
+      },
+      {
+        topic: "Animal Nutrition",
+      },
+      {
+        topic: "US elections",
+      },
+      {
+        topic: "Maritime shipping",
+      },
+      {
+        topic: "Switzerland",
+      },
+    ]);
+
+    res.json({ status: "ok", msg: "seeding successful" });
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).json({ status: "error", msg: "seeding error" });
+  }
+};
+
+// get all collections
+const getAllSmartCollections = async (req, res) => {
+  try {
+    const allCollections = await SmartCollectionsModel.find();
+    res.json(allCollections);
+  } catch (error) {
+    console.log(error.message);
+    res
+      .status(400)
+      .json({ status: "error", msg: "error getting all collections" });
+  }
+};
+
+// add new collection
+const addSmartCollection = async (req, res) => {
+  try {
+    const newCollection = {};
+    await SmartCollectionsModel.create(newCollection);
+
+    res.json({ status: "ok", msg: "collection saved" });
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).json({ status: "error", msg: "collection not saved" });
+  }
+};
+
+// delete collection
+
+// get collections by user (Object) ID
+
+// get content of one collection
+
+module.exports = { seedSmartCollection, getAllSmartCollections };
--- backend/src/models/SmartCollections.js
+++ backend/src/models/SmartCollections.js
@@ -2,10 +2,18 @@ const mongoose = require("mongoose");
 
 const SmartCollectionsSchema = new mongoose.Schema(
   {
-    // title: { type: String, require: true, minLength: 1, maxLength: 50 },
-    // author: { type: String, require: true, minLength: 1, maxLength: 50 },
-    // year_published: { type: Number, require: true, min: 1900 },
-    // created_at: { type: Date, default: Date.now },
+    // users: {type: mongoose.Schema.Types.ObjectId
+    // ref: "Auth"}
+    topic: { type: String, required: true, minLength: 1, maxLength: 20 },
+    from: {
+      type: String,
+      default: () => {
+        const date = Date.now() - 14 * 24 * 60 * 60 * 1000;
+        return new Date(date).toLocaleString();
+      },
+    },
+    minClusterSize: { type: Number, min: 5 },
+    sortBy: { type: String, default: "createdAt" },
   },
   { collection: "smartCollections" }
 );
