6690b18e0a476d4100967577ee33718f44956e38
samanthatan01 <samanthatan6496@gmail.com>
2024-04-04 09:55:23 +0800

    Update SmartCollections CRUD
    Addition of PATCH and DELETE of SmartCollection

--- backend/src/routers/smartCollections.js
+++ backend/src/routers/smartCollections.js
@@ -3,11 +3,15 @@ const {
   seedSmartCollection,
   getAllSmartCollections,
   addSmartCollection,
+  patchSmartCollection,
+  deleteSmartCollection,
 } = require("../controllers/smartCollections");
 const router = express.Router();
 
 router.get("/seed", seedSmartCollection);
 router.get("/", getAllSmartCollections);
 router.put("/", addSmartCollection);
+router.patch("/:id", patchSmartCollection);
+router.delete("/:id", deleteSmartCollection);
 
 module.exports = router;
