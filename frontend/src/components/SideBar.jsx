21132c474a3fd67ff07aa424735f8d80c2db9527
mdinizreis <m4rcosdiniz@gmail.com>
2024-04-06 15:04:55 +0800

    Added Feature to expand/collapse Sidebar

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -10,7 +10,7 @@
  |
  *===========================================================================*/
 
-import React from "react";
+import React, { useState } from "react";
 import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
 import styles from "./SideBar.module.css";
 
@@ -21,25 +21,41 @@ import WorkOutlineOutlinedIcon from "@mui/icons-material/WorkOutlineOutlined";
 import AccountBalanceOutlinedIcon from "@mui/icons-material/AccountBalanceOutlined";
 import MemoryOutlinedIcon from "@mui/icons-material/MemoryOutlined";
 import AttractionsOutlinedIcon from "@mui/icons-material/AttractionsOutlined";
+import MenuOpenOutlinedIcon from "@mui/icons-material/MenuOpenOutlined";
 
 const SideBar = () => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const toggleSidebar = () => {
+    setCollapsed(!collapsed);
+  };
   return (
-    <Sidebar className={styles.sidebar}>
+    <Sidebar
+      className={styles.sidebar}
+      collapsed={collapsed}
+      onToggle={toggleSidebar}
+    >
       <Menu>
-        <div className={styles.sidebar}>
-          <p>Top Articles</p>
-        </div>
-        <SubMenu prefix={<AttachMoneyOutlinedIcon />} label="Finance">
+        <MenuItem
+          icon={<MenuOpenOutlinedIcon />}
+          onClick={() => {
+            toggleSidebar();
+          }}
+        >
+          {" "}
+          <p className={styles.sidebar}>Top Articles</p>
+        </MenuItem>
+        <SubMenu icon={<AttachMoneyOutlinedIcon />} label="Finance">
           {" "}
           <MenuItem>Test</MenuItem>
           <MenuItem>Test 2</MenuItem>
           <MenuItem>Test 3</MenuItem>
         </SubMenu>
-        <MenuItem prefix={<DirectionsRunOutlinedIcon />}> Sports</MenuItem>
-        <MenuItem prefix={<WorkOutlineOutlinedIcon />}> Business</MenuItem>
-        <MenuItem prefix={<AccountBalanceOutlinedIcon />}> Politics</MenuItem>
-        <MenuItem prefix={<MemoryOutlinedIcon />}> Tech</MenuItem>
-        <MenuItem prefix={<AttractionsOutlinedIcon />}> Entertainment</MenuItem>
+        <MenuItem icon={<DirectionsRunOutlinedIcon />}> Sports</MenuItem>
+        <MenuItem icon={<WorkOutlineOutlinedIcon />}> Business</MenuItem>
+        <MenuItem icon={<AccountBalanceOutlinedIcon />}> Politics</MenuItem>
+        <MenuItem icon={<MemoryOutlinedIcon />}> Tech</MenuItem>
+        <MenuItem icon={<AttractionsOutlinedIcon />}> Entertainment</MenuItem>
       </Menu>
     </Sidebar>
   );
