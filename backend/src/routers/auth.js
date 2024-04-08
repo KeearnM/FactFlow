b6e0d287c1ea6645ef9c452a96cb73574fea9660
keearn <keearn96@gmail.com>
2024-04-08 10:31:13 +0800

    added protection to routes

--- backend/src/routers/auth.js
+++ backend/src/routers/auth.js
@@ -13,14 +13,15 @@ const {
   validateRegister,
   validateRefresh,
 } = require("../validators/auth");
+const { authAdmin, authUser } = require("../middleware/auth");
 
 const router = express.Router();
 
 router.put("/register", validateRegister, errorCheck, register);
 router.post("/login", validateLogin, errorCheck, login);
-router.post("/refresh", validateRefresh, errorCheck, refresh);
+router.post("/refresh", authUser, validateRefresh, errorCheck, refresh);
 router.get("/seed", seedUsers);
-router.get("/allUser", getAllAuth);
-router.patch("/update/:id", validateRegister, errorCheck, update);
+router.get("/allUser", authAdmin, getAllAuth);
+router.patch("/update/:id", authAdmin, validateRegister, errorCheck, update);
 
 module.exports = router;
--- backend/src/routers/smartCollections.js
+++ backend/src/routers/smartCollections.js
@@ -13,12 +13,14 @@ const {
   validateUpdateSmartCollectionData,
 } = require("../validators/smartCollections");
 const { errorCheck } = require("../validators/errorCheck");
+const { authUser } = require("../middleware/auth");
 const router = express.Router();
 
 router.get("/seed", seedSmartCollection);
 router.get("/", getAllSmartCollections);
 router.put(
   "/:id",
+  authUser,
   validateIdInParam,
   validateAddSmartCollectionData,
   errorCheck,
@@ -26,12 +28,25 @@ router.put(
 ); // add collection to a specific auth object ID -- in req params or body?
 router.patch(
   "/:id",
+  authUser,
   validateIdInParam,
   validateUpdateSmartCollectionData,
   errorCheck,
   patchSmartCollection
 );
-router.delete("/:id", validateIdInParam, errorCheck, deleteSmartCollection);
-router.get("/:id", validateIdInParam, errorCheck, getCollectionByUserID);
+router.delete(
+  "/:id",
+  authUser,
+  validateIdInParam,
+  errorCheck,
+  deleteSmartCollection
+);
+router.get(
+  "/:id",
+  authUser,
+  validateIdInParam,
+  errorCheck,
+  getCollectionByUserID
+);
 
 module.exports = router;
