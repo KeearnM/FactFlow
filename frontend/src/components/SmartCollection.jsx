aca9104d958288bd474558edf8030125790aba26
samanthatan01 <samanthatan6496@gmail.com>
2024-04-09 13:13:53 +0800

    Update SmartCollection.jsx
    Collapsible feature for CRUD

--- frontend/src/components/SmartCollection.jsx
+++ frontend/src/components/SmartCollection.jsx
@@ -2,8 +2,14 @@ import React, { useEffect, useState } from "react";
 import { useContext } from "react";
 import UserContext from "../context/user";
 import useFetch from "../hooks/useFetch";
+import styles from "../components/SmartCollection.module.css";
 
-import { TextField, Box, Button } from "@mui/material";
+import { TextField, Box, Button, Grid } from "@mui/material";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import ExpandMoreOutlinedIcon from "@mui/icons-material/ExpandMoreOutlined";
+import IconButton from "@mui/material/IconButton";
+import { styled } from "@mui/material/styles";
 
 const SmartCollection = () => {
   const userCtx = useContext(UserContext);
@@ -14,6 +20,25 @@ const SmartCollection = () => {
   console.log(userCtx.smartCollection);
   console.log(userCtx.loggedUserId);
   console.log(newCollection);
+
+  const ExpandMore = styled((props) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+  })(({ theme, expand }) => ({
+    transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+    }),
+  }));
+
+  //Control card expansion for Advanced Search Options
+  const [expanded, setExpanded] = useState(false);
+
+  //handle Edit Collection Expand click
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
 
   // get smart collection for the specific logged-in user
   const getCollectionByUserID = async () => {
@@ -75,6 +100,54 @@ const SmartCollection = () => {
           Add
         </Button>
       </Box>
+      <CardActions>
+        <small>Edit Collection</small>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreOutlinedIcon />
+        </ExpandMore>
+      </CardActions>
+
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <Grid container spacing={2}>
+          <Grid item xs={6} md={6}>
+            {userCtx.smartCollection.map((item) => {
+              return (
+                <>
+                  <label className="col-md-3">{item.q}</label>
+                  <Button
+                    className="col-md-2"
+                    variant="contained"
+                    style={{
+                      marginLeft: 5,
+                      height: "28px",
+                      backgroundColor: "#1976D2",
+                    }}
+                  >
+                    Update
+                  </Button>
+                  <Button
+                    className="col-md-2"
+                    variant="contained"
+                    style={{
+                      marginLeft: 5,
+                      height: "28px",
+                      backgroundColor: "black",
+                    }}
+                  >
+                    Delete
+                  </Button>
+                  <br />
+                </>
+              );
+            })}
+          </Grid>
+        </Grid>
+      </Collapse>
     </>
   );
 };
