66845bff665cb8ec60b28f2d669638fb3cea8a0a
samanthatan01 <samanthatan6496@gmail.com>
2024-04-09 17:12:50 +0800

    Update smartCollection.jsx

--- frontend/src/components/SmartCollection.jsx
+++ frontend/src/components/SmartCollection.jsx
@@ -10,11 +10,14 @@ import Collapse from "@mui/material/Collapse";
 import ExpandMoreOutlinedIcon from "@mui/icons-material/ExpandMoreOutlined";
 import IconButton from "@mui/material/IconButton";
 import { styled } from "@mui/material/styles";
+import UpdateCollectionModal from "./UpdateCollectionModal";
 
 const SmartCollection = () => {
   const userCtx = useContext(UserContext);
   const fetchData = useFetch();
   const [newCollection, setNewCollection] = useState("");
+  const [showUpdateCollectionModal, setShowUpdateCollectionModal] =
+    useState(false);
 
   // for testing purposes -- to delete when completed
   console.log(userCtx.smartCollection);
@@ -32,12 +35,22 @@ const SmartCollection = () => {
     }),
   }));
 
-  //Control card expansion for Advanced Search Options
+  // Control card expansion for Advanced Search Options
   const [expanded, setExpanded] = useState(false);
 
-  //handle Edit Collection Expand click
+  // handle Edit Collection Expand click
   const handleExpandClick = () => {
     setExpanded(!expanded);
+  };
+
+  // open update modal
+  const handleOpenUpdateModal = () => {
+    setShowUpdateCollectionModal(true);
+  };
+
+  // close update modal
+  const handleCloseUpdateModal = () => {
+    setShowUpdateCollectionModal(false);
   };
 
   // get smart collection for the specific logged-in user
@@ -71,7 +84,6 @@ const SmartCollection = () => {
     if (res.ok) {
       getCollectionByUserID();
       setNewCollection([]);
-      alert(JSON.stringify(`Collection has been added.`));
     } else {
       alert(JSON.stringify(res.data));
       console.log(res.data);
@@ -133,9 +145,19 @@ const SmartCollection = () => {
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <Grid container spacing={2}>
           <Grid item xs={6} md={6}>
-            {userCtx.smartCollection.map((item) => {
+            {userCtx.smartCollection.map((item, index) => {
+              console.log(userCtx.smartCollection);
               return (
                 <>
+                  {showUpdateCollectionModal && (
+                    <UpdateCollectionModal
+                      key={index}
+                      id={item._id}
+                      q={item.q}
+                      getCollectionByUserID={getCollectionByUserID}
+                      handleCloseUpdateModal={handleCloseUpdateModal}
+                    />
+                  )}
                   <label className="col-md-3">{item.q}</label>
                   <Button
                     value={item._id}
@@ -146,6 +168,7 @@ const SmartCollection = () => {
                       height: "28px",
                       backgroundColor: "#1976D2",
                     }}
+                    onClick={handleOpenUpdateModal}
                   >
                     Update
                   </Button>
