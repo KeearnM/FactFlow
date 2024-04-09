42f6c70c5ed647b1c53a7348708a955b5726f788
samanthatan01 <samanthatan6496@gmail.com>
2024-04-09 11:48:43 +0800

    Update validators on backend for Topic and Q parameters

--- backend/src/validators/smartCollections.js
+++ backend/src/validators/smartCollections.js
@@ -11,16 +11,16 @@ const validateIdInBody = [
 ];
 
 const validateAddSmartCollectionData = [
-  body("topic", "topic is required").not().isEmpty(),
-  body("topic", "must have a min of 1 and a max of 30 characters").isLength({
+  body("q", "input is required").not().isEmpty(),
+  body("q", "must have a min of 1 and a max of 30 characters").isLength({
     min: 1,
     max: 30,
   }),
 ];
 
 const validateUpdateSmartCollectionData = [
-  body("topic", "topic is required").not().isEmpty(), // optional() is to validate for optional parts of the request payload
-  body("topic", "must have a min of 1 and a max of 30 characters").isLength({
+  body("q", "input is required").not().isEmpty(), // optional() is to validate for optional parts of the request payload
+  body("q", "must have a min of 1 and a max of 30 characters").isLength({
     min: 1,
     max: 30,
   }),
